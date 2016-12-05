471d8318e443dbcf5d6936165acb7d7f47aa9dbc
dnorwood112870 <dnorwood112870@gmail.com>
2016-12-05 05:05:04 +0000

    phpProject 12/05/2016

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ echo "<blockquote>";
 echo "Hello, welcome to my first PHP page!!! This page is designed to ENLIGHTENED the U.S. GOVERNMENT about iSecurity using PHP!!! ";
 echo "I AM a TOP SECRET GOVERNMENT iServer named <br/><br/> iRootServer.iSercurity.gov, www.iSecurity.gov, or wwwroot.iSecurity.gov.";
 echo "The 'i' for Internet in my name, represents my SUPERIOR presence on the Internet.  I AM the ROOT Internet Server <br/></br> to the newly established <strong>U.S. DEPARTMENT OF INTERNET SECURITY.</strong><br/><br/>";
-echo "<a href='/controllers/guestBook.php'>Sign Guestbook</a>";
+echo "<a href='/controller/guestBook.php'>Sign Guestbook</a>";
 echo "</blockquote>";
 ?>
 </div>
